53d89481a846a9e226031e4e8c81f01f968f563d
Rafael Araujo <rafael3124@gmail.com>
2023-02-14 23:19:15 -0300

    atividade

---
+++ variaveis.js
@@ -0,0 +1,15 @@
+//var
+var x = 0
+var x = 'Renato'
+
+console.log(x)
+
+//let
+let y = (0)
+//let y = 'Carlos' não permite redeclarar valor
+
+console.log(y)
+
+//constante
+const ano = '2023'
+console.log(ano)
