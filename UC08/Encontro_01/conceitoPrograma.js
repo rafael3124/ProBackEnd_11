0d77fbdc5c1ffd50178a82f6ea1426d939584dc2
Rafael Araujo <rafael3124@gmail.com>
2023-02-14 23:00:56 -0300

    atividade

--- UC08/Encontro_01/conceitoPrograma.js
+++ UC08/Encontro_01/conceitoPrograma.js
@@ -3,7 +3,7 @@ let numero1 = 14
 let numero2 = 36
 
 //processamento
-let resultado - numero1 + numero2
+let resultado = numero1 + numero2
 
 //saída
 console.log(resultado)
---
+++ UC08/Encontro_01/tiposDeDados.js
@@ -0,0 +1,18 @@
+//tipos de dadaos
+
+//number
+let idade = 100 // recebe valor numerico 100
+
+//string
+let nome = 'Renan' //recebe valor de texto Renan
+
+//objeto
+let obj = {primeiroNome: 'Renan', sobrenome:'Silva'}
+
+//array(lista)
+let lista = [0,3,5,8, 'André', 'Mayck']
+
+console.log(idade)
+conseole.log(nome)
+console.log(obj)
+console.log(lista)
