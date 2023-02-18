6beb1e15e5c46bc20320d3c3364a4f92f3c6c90e
Rafael Araujo <rafael3124@gmail.com>
2023-02-18 20:26:33 -0300

    EncontroRemoto2

--- UC09/SA2/EncontroRemoto2/Classes/PessoaFisica.cs
+++ UC09/SA2/EncontroRemoto2/Classes/PessoaFisica.cs
@@ -2,15 +2,21 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EncontroRemoto2.Interfaces;
 
 namespace EncontroRemoto2.Classes
 {
-    public class PessoaFisica : Pessoa
+    public class PessoaFisica : Pessoa, IPessoaFisica
     {
         public string? Cpf { get; set; }
         public DateTime DataNascimento { get; set; }
 
         public override float PagarImposto(float rendimento)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ValidarDataNascimento(DateTime dataNascimento)
         {
             throw new NotImplementedException();
         }
--- UC09/SA2/EncontroRemoto2/Classes/PessoaJuridica.cs
+++ UC09/SA2/EncontroRemoto2/Classes/PessoaJuridica.cs
@@ -2,15 +2,21 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EncontroRemoto2.Interfaces;
 
 namespace EncontroRemoto2.Classes
 {
-    public class PessoaJuridica : Pessoa
+    public class PessoaJuridica : Pessoa, IPessoaJuridica
     {
         public string? RazaoSocial { get; set; }
         public string? Cnpj { get; init; }
 
         public override float PagarImposto(float rendimento)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool ValidarCnpj(float rendimento)
         {
             throw new NotImplementedException();
         }
