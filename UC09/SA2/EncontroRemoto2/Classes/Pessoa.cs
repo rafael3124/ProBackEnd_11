b6cd1262e67cc42d2dcee31c39a6f5e9bf397be3
Rafael Araujo <rafael3124@gmail.com>
2023-02-18 20:07:49 -0300

    EncontroRemoto2

--- UC09/SA2/EncontroRemoto2/Classes/Pessoa.cs
+++ UC09/SA2/EncontroRemoto2/Classes/Pessoa.cs
@@ -2,14 +2,19 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using EncontroRemoto2.Interfaces;
 
 namespace EncontroRemoto2.Classes
 {
-    public abstract class Pessoa
+    public abstract class Pessoa : IPessoa
     {
         public string? Nome { get; set; }
         public string? Endereco { get; set; }
         public float Rendimento { get; set; }
 
+        public float PagarImposto(float rendimento)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
