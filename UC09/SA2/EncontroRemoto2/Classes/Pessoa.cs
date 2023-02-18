895d7e7ec33f39a10618c27e008ea0ce23604286
Rafael Araujo <rafael3124@gmail.com>
2023-02-18 19:48:24 -0300

    EncontroRemoto2

---
+++ UC09/SA2/EncontroRemoto2/Classes/Pessoa.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EncontroRemoto2.Classes
+{
+    public class Pessoa
+    {
+        public string? Nome { get; set; }
+        public string? Endereco { get; set; }
+        public float Rendimento { get; set; }
+
+    }
+}
---
+++ UC09/SA2/EncontroRemoto2/Classes/PessoaFisica.cs
@@ -0,0 +1,13 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace EncontroRemoto2.Classes
+{
+    public class PessoaFisica
+    {
+        public string? Cpf { get; set; }
+        public DateTime DataNascimento { get; set; }
+    }
+}
