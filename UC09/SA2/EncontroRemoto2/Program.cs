347d99fb888a828e4e8487efbf82f3e8da0d3533
Rafael Araujo <rafael3124@gmail.com>
2023-02-18 22:11:02 -0300

    EncontroRemoto2

--- UC09/SA2/EncontroRemoto2/Program.cs
+++ UC09/SA2/EncontroRemoto2/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using EncontroRemoto2.Classes;
+
+//instanciar um objeto da classe PessoaFisica
+PessoaFisica novaPf = new PessoaFisica();
+
+novaPf.Nome = "Rafael";
+novaPf.Rendimento = 50000.58f; 
+
+Console.WriteLine(novaPf.Nome);
+Console.WriteLine(novaPf.Rendimento);
+
+
+PessoaJuridica novaPj = new PessoaJuridica();
+
+novaPj.RazaoSocial = "SENAI Informática";
+
+Console.WriteLine($"Razão Social: {novaPj.RazaoSocial}");
